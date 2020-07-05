6e11cd15a49f8e74185f0dd4d281c5499cb7f329
Imgopz <rrgkmech@gmail.com>
2020-07-06 03:10:59 +0530

    navigate between screens

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-const CategoryMealsScreen = props => {
-    return (
-        <View>
-            <Text>The category Meals screen!!</Text>
-        </View>
-    );
-}
+const CategoryMealScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Category Meal Screen!</Text>
+      <Button title="Go to Details" onPress={() => {
+          props.navigation.navigate({
+              routeName: 'MealDetail'
+          });
+      }} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
-export default CategoryMealsScreen;
+export default CategoryMealScreen;
